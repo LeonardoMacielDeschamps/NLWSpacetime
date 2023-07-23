cbcb86d22d7f49bde7cc5eb257c9cee9b15357e6
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-07-22 21:07:26 -0300

    Aula 03

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,29 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
-
-app.register(memoriesRoutes)
 
 app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: '45a0a6b0cc819acbadca4debe039d3d75c9aa8d3',
+})
+
+app.register(memoriesRoutes)
+app.register(authRoutes)
+
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('Server is running! 🚀')
