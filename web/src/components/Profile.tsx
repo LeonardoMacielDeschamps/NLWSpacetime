add212f385aa9f0c983aade15875c0212993b360
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-07-22 22:55:52 -0300

    Aula 04

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -16,8 +16,11 @@ export function Profile() {
 
       <p className="max-w-[140px] whitespace-nowrap text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
-          Quero sair
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
+          Sair
         </a>
       </p>
     </div>
